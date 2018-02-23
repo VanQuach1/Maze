c02cff3a9aa89b8b9a1cea74c090b42b6cd9a4f2
vanquach1 <van.quach@outlook.com>
2018-02-23 15:53:12 -0600

    Maze Project

---
+++ PeopleNet/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Maze.Controllers
+{
+    [Route("api/[controller]")]
+    public class SolveMazeController : ControllerBase
+    {
+        // GET api/SolveMaze
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/SolveMaze/5
+        [HttpGet("{id}")]
+        public void Get(int id)
+        {
+        }
+        
+        // POST api/SolveMaze
+        [HttpPost]
+        public Value Post([FromBody] Value value)
+        {
+            string solution = new MazeCalculator(value.Map).RetriveRoute();
+            value.Solution = solution;
+            value.Steps = solution.Split('@').Length;
+
+            return value;
+        }
+
+        // PUT api/SolveMaze/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/SolveMaze/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Value
+    {
+        public string Map { get; set; }
+        public int Steps { get; set; }
+        public string Solution { get; set; }
+    }
+}
